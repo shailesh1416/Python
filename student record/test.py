4da2f76990d0d6cbd783ae923e7b6ea42bdf9dc5
akhileshyadav26 <shailesh1416@gmail.com>
2022-08-23 18:10:48 +0530

    test removed

--- student record/test.py
+++
@@ -1,8 +0,0 @@
-file = open('database2', 'r')
-
-theRecords = list()
-student = file.readline().rstrip()
-for i in range(2):
-    theRecords.append(student)
-    student = file.readline().rstrip()
-print(theRecords)
